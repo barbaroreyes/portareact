c26e72787019564889e47404069b94e29b321f7e
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-04-09 15:04:31 -0500

    update

--- src/Compo/Header.js
+++ src/Compo/Header.js
@@ -27,7 +27,9 @@ const Header = () => {
           </li>
          </ul>
         </nav>
-      <div>foto</div>
+      <div className="imageContainer">
+        <img  className="picture"src ='https://images.unsplash.com/photo-1576836165612-8bc9b07e7778?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt=""/>
+        </div>
       <div
       onClick={() =>all()}
       style={{flexDirection:line?
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="home">
-      <div className="picture">
+      {/* <div className="picture">
        picture
-      </div>
+      </div> */}
       <div className="dadproject">
       <h1>Software </h1>
         <div className='kidproject'>Full-Stack</div>
