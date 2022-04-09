64833a59f4997c80f90eb7a762bf8901ad32118a
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-04-08 22:34:20 -0500

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,32 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="home">
-      <div className="picture">picture</div>
+      <div className="picture">
+        <img src='https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt="" />
+      </div>
       <div className="dadproject">
         <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+        <div className='kidproject'>projec</div>
+       
         {/* <div className='kidproject'>projec</div> */}
         {/* <div className='kidproject'>projec</div>
-        <div className='kidproject'>projec</div>
-        <div className='kidproject'>projec</div>
-        <div className='kidproject'>projec</div>
-        <div className='kidproject'>projec</div>
-        <div className='kidproject'>projec</div>
-        <div className='kidproject'>projec</div>
+        
         <div className='kidproject'>projec</div> */}
        
 
