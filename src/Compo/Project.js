761108fd3b84b432d7bdc20adbd692b0afc4d462
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-04-09 17:30:44 -0500

    update

--- src/Compo/Project.js
+++ src/Compo/Project.js
@@ -7,8 +7,7 @@ const Project = (props) => {
     <div className="card-project">
        <div className="card">
        <h3>{props.name}</h3>
-       <h3>{props.tecnologies}</h3>
-        <h4>{props.category}</h4>
+       
         <div className = "logo">
         <img className="logoImage" src = {props.description} alt=''/>
         </div>
